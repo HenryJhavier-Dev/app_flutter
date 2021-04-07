f50c5fdea343ec5d7f892591382ed551f1bbde3f
HenryJhavier-Dev <>
2021-04-07 21:29:32 +0200

    Corrigiendo error

--- android/app/src/main/kotlin/com/hjrodev/app_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/hjrodev/app_flutter/MainActivity.kt
@@ -5,10 +5,10 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
-    override fun Oncreate(saveInstanceState: Bundle?){
+   /* override fun Oncreate(saveInstanceState: Bundle?){
         super.onCreate(saveInstanceState)
         GeneratedPluginRegistrant.registerWith(tris)
         // todo hace invisible el status bar
         this.getWindows().setStatusBarColor(android.graphics.Color.TRANSPARENT)
-    }
+    }*/
 }
